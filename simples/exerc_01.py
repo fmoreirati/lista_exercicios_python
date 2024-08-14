08fc7749538a19c9f92bdb177f51c315634ae6e7
fMoreira <fabianomoreira.prof@gmail.com>
2024-08-14 19:25:33 -0300

    questões 01 e 07

---
+++ simples/exerc_01.py
@@ -0,0 +1,9 @@
+# 1) Escreva um programa para ler o raio de um círculo, calcular e escrever a sua área (Pi * (raio * raio))
+
+#entrada
+raio = int(input("Qual o raio?"))
+PI = 3.14 #constante
+#processamento
+area = PI * (raio * raio)
+#saida
+print("Area do circulo ", area)
---
+++ simples/exerc_07.py
@@ -0,0 +1,21 @@
+''' 7) A equipe Benneton-Ford deseja calcular o número mínimo de litros que deverá colocar no tanque de
+seu carro para que ele possa percorrer um determinado número de voltas até o primeiro
+reabastecimento. Escreva um programa que leia o comprimento da pista (em metros), o número total de
+voltas a serem percorridas no grande prêmio, o número de abastecimentos desejados e o consumo de
+combustível do carro (em Km/L). Calcular e escrever o número mínimo de litros necessários para
+percorrer até o primeiro reabastecimento. OBS: Considere que o número de voltas entre os
+abastecimentos é o mesmo.'''
+
+#entrada
+comprimento = float(input("Comprimento da pista (em metros)? ")) #1000 -> 1km
+voltas = int(input("Número total devoltas? ")) #10
+abastecimento = int(input("Número de abastecimentos desejados? ")) #2
+consumo = float(input("Consumo de combustível do carro (em Km/L)? ")) #10 
+
+#processamento
+kmTotal = (comprimento / 1000) * voltas
+consumoTotal = kmTotal / consumo
+litros = consumoTotal / abastecimento
+
+#saida
+print("O número mínimo de litros necessários até o primeiro reabastecimento: ", litros)
