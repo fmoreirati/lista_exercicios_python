5edbabf830081b9d38ca576d3e98467d6726ccb0
fMoreira <fabianomoreira.prof@gmail.com>
2024-08-28 20:45:22 -0300

    sorteio de valores

--- repeticao/exerc-8-76-random.py
+++ repeticao/exerc-8-76-random.py
@@ -6,45 +6,30 @@
 #OBS1. Refaça o exercícios ussando lista para armazenaros valores digitados pelo usuario.
 #OBS2. Faça com que o sistema sorteie 6 valores digitados pelo usuarios logo após o exibir as mensagens (random.randrange(1, 51))
 
+import random
+
 maior = 0
 menor = 9999999
 soma = 0
-limite = 5
-
+limite = 10
 listValores = [] #[] respresta um Array ou Lista
 
 for x in range(1, limite+1):
-    valor = int(input(f"Qual o {x}º valor?"))
+    #valor = int(input(f"Qual o {x}º valor?"))
+    valor = random.randrange(1, 101)
     if(valor > maior):
         maior = valor
     if(valor < menor):
         menor = valor
     soma = soma + valor
-    print("Soma: ", soma)
+
     listValores.append(valor)
 
+print("Soma: ", soma)
 print("Media: ", soma/limite)
 print("Maior: ", maior)
 print("Menor: ", menor)
 print("Valores: ", listValores)
 
-
-for itemValor in listValores:
-    if(itemValor % 2 == 0):
-        print("Par: ", itemValor)
-
-for itemValor in listValores:
-    if(itemValor % 2 != 0):
-        print("Impar: ", itemValor)
-
-listPar = []
-listImpar = []
-
-for itemValor in listValores:
-    if(itemValor % 2 == 0):
-       listPar.append(itemValor)
-    else:
-       listImpar.append(itemValor) 
-
-print("Pares: ", listPar)
-print("Impares: ", listImpar)
+index = random.randrange(0,10)
+print("Numero sorteado:(",index,") ", listValores[index])
