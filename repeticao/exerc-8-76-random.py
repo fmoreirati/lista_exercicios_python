bfa6d5668384bc799a1d766e18a274d2046cedd8
fMoreira <fabianomoreira.prof@gmail.com>
2024-08-28 22:15:29 -0300

    Sorteio de numerios

--- repeticao/exerc-8-76-random.py
+++ repeticao/exerc-8-76-random.py
@@ -31,5 +31,21 @@ print("Maior: ", maior)
 print("Menor: ", menor)
 print("Valores: ", listValores)
 
-index = random.randrange(0,10)
-print("Numero sorteado:(",index,") ", listValores[index])
+
+# Sorteio de Valores
+listSorteados = []
+quantNumerosSorteio = 4
+
+# Comando While (Enquanto): utilizado para repetir entonto a expressao for veraderia. Neste caso enquanto a lista de sorteador for menor que quantidade de numeros sorteados
+# Comando Len (lenght = tamanho): retorna o valor do tamanho da lista, isto é quantidade de itens
+while(len(listSorteados) < quantNumerosSorteio): 
+    index = random.randrange(0, len(listValores))
+    valorSorteado = listValores[index]
+    contDuplicados = 0
+    for s in listSorteados:
+        if(s==valorSorteado):
+            contDuplicados += 1
+    if(contDuplicados == 0):
+        listSorteados.append(valorSorteado)
+
+print("Numero sorteado: ", listSorteados )
